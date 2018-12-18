5c9a013663740fa60fd78e5a8c2c3b253e2860d5
Jee Wen Jie <40534826+jeewenjie@users.noreply.github.com>
2018-12-18 15:29:58 +0800

    Add files via upload

--- traintest_split_script.py
+++ traintest_split_script.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Dec 13 15:32:57 2018
 
-@author: Wen Jie
+@author: audiodsp
 """
 
 import pandas as pd
@@ -10,10 +10,11 @@ import numpy as np
 
 # Ratio to split data into train/test
 cutoff = 0.8
-traintest_cutoff = int(np.ceil(cutoff*50000))
 
-df = pd.read_csv('\path')
+# header = None makes it so that it won't take the first row and make it the header
+df = pd.read_csv('/home/audiodsp/Desktop/traintest.csv',header=None)
 
+traintest_cutoff = int(np.ceil(cutoff*df.shape[0]))
 # frac is an argument that returns the fractional number of total rows randomly
 # So frac=1 means it returns everything randomly. reset_index renumbers the 
 # indices to running order. Can set to False if unimportant/
@@ -22,3 +23,7 @@ df = df.sample(frac=1).reset_index(drop=True)
 # :traintest_cutoff = traintest_cutoff
 # traintest_cutoff: = 1-traintest_cutoff
 train,test = df[:traintest_cutoff],df[traintest_cutoff:]
+
+# header = False makes it so that it won't write the headers as the first row
+train.to_csv('/home/audiodsp/Desktop/train.csv',header=False,index=False)
+test.to_csv('/home/audiodsp/Desktop/test.csv',header=False,index=False)
